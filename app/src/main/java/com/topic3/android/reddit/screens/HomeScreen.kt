e715f94e8ce9661d0153e3520f9434e7ff90b731
nastyahh <nasstyahus@gmail.com>
2024-05-03 17:20:03 +0300

    add TrendingTopics to the main screen

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -83,19 +83,39 @@ fun HomeScreen(viewModel: MainViewModel) {
             Timer().schedule(3000) { isToastVisible = false }
         }
     }
+    val homeScreenItems = mapHomeScreenItems(posts)
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
-            modifier = Modifier.background(color = MaterialTheme.colors.secondary)
-        ) {
-            items(posts) {
-                if (it.type == PostType.TEXT) {
-                    TextPost(it, onJoinButtonClick = onJoinClickAction)
-                } else {
-                    ImagePost(it, onJoinButtonClick = onJoinClickAction)
-                }
-                Spacer(modifier = Modifier.height(6.dp))
+            modifier = Modifier.background(color = MaterialTheme.colors.secondary),
+            content= {
+                items(
+                    items = homeScreenItems,
+                    itemContent = { item ->
+                        if (item.type == HomeScreenItemType.TRENDING) {
+                            TrendingTopics(
+                                trendingTopics = trendingItems,
+                                modifier = Modifier.padding(
+                                    top = 16.dp, bottom = 6.dp
+                                )
+                            )
+                        } else if (item.post != null) {
+                            val post = item.post
+                            if (post.type == PostType.TEXT) {
+                                TextPost(
+                                    post = post,
+                                    onJoinButtonClick = onJoinClickAction
+                                )
+                            } else {
+                                ImagePost(
+                                    post = post,
+                                    onJoinButtonClick = onJoinClickAction
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(6.dp))
+                        }
+                    })
             }
-        }
+        )
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
@@ -155,6 +175,23 @@ private fun TrendingTopics(
             )
         }
     }
+}
+
+private fun mapHomeScreenItems(
+    posts: List<PostModel>
+):List<HomeScreenItem>{
+    val homeScreenItems= mutableListOf<HomeScreenItem>()
+// Add Trending item
+            homeScreenItems.add(
+                HomeScreenItem(HomeScreenItemType.TRENDING)
+            )
+// Add Post items
+    posts.forEach { post ->
+        homeScreenItems.add(
+            HomeScreenItem(HomeScreenItemType.POST, post)
+        )
+    }
+                    return homeScreenItems
 }
 
 private data class HomeScreenItem(
